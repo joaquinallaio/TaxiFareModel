89f5b6a36daaf4f0f6a749f42bd13935b02fe312
joaquinallaio <joaco.allaio@gmail.com>
2022-05-31 17:44:29 -0300

    train to vs

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,74 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from data import get_data , clean_data
+from encoders import  TimeFeaturesEncoder , DistanceTransformer
+from utils import compute_rmse
+from sklearn.model_selection import train_test_split
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    # implement set_pipeline() function
+    def set_pipeline(self):
+        '''returns a pipelined model'''
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+        return self.pipeline
+
+    # implement train() function
+
+    def run(self):
+        '''returns a trained pipelined model'''
+        self.set_pipeline()
+        self.pipeline.fit(self.X , self.y)
+        return
+
+    def evaluate(self):
+        """evaluates the pipeline on df_test and return the RMSE"""
+    # implement evaluate() function
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        '''returns the value of the RMSE'''
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        return rmse
+
+
+if __name__ == "__main__":
+    # get data
+    df = get_data()
+    # clean data
+    df = clean_data(df)
+    # set X and y
+    X = df
+    y = df.pop("fare_amount")
+    # hold out
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+    # train
+    trainer = Trainer(X_train,y_train)
+    trainer.run()
+    # evaluate
+    trainer.evaluate(X_val,y_val)
